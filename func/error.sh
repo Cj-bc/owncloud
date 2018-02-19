ca8e255f0fa5d44ff9a3f0cb266a67741e45b931
Cj <cj-bc@y-modify.org>
2018-02-19 23:10:40 +0900

    Add: error.sh
    which aims to output error

---
+++ func/error.sh
@@ -0,0 +1,21 @@
+
+#! /bin/bash
+#
+# error.sh  -- library for owncloud.sh
+#              Related with error treatment
+#
+# copyright (c) 2018 Cj-bc
+#
+# This software is relesed under MIT Lisence.
+# http://opensource.org/license/mit-license.php
+
+# input: <error_message> <error_code>
+# aim: output error to console
+
+errorcode=$1;errorMessage=$2;
+
+cat <<EOT
+  error has occured.
+  Error code: $errorcode
+  Message: $errorMessage
+EOT

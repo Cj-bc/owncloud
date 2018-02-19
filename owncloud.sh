95e98107210d26c8c03f6abb39e93a69ba9e38d4
Cj <cj-bc@y-modify.org>
2018-02-19 17:49:15 +0900

    Add: main shell script, owncloud.sh

---
+++ owncloud.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+#
+# owncloud  -- a simplest owncloud client for CLI
+#
+# copyright (c) 2018 Cj-bc
+#
+# This software is relesed under MIT Lisence.
+# http://opensource.org/license/mit-license.php
+
+version=0.0.1
+
+OWNCLOUD_PATH="${brew --prefix}/Cellar/owncloud/${version}/"
+
+source ${OWNCLOUD_PATH}/config

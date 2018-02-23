bb5a0dc3938b5a8f29a97a791295d2d3058f91b3
Cj <cj-bc@y-modify.org>
2018-02-23 20:57:44 +0900

    Change: added announce text before each test.
    "test start: ..."

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "this is test file!!!! hello world" > test.txt
-./owncloud.sh post test.txt testdir/test.txt || echo "owncloud.sh post ERROR"
-./owncloud.sh ls testdir || echo "owncloud.sh ls ERROR:"
-./owncloud.sh get testdir/test.txt || echo "owncloud.sh get ERROR:"
+echo "this is test file!!!! hello world" > test.txt && echo "test: test.txt generated"
+echo "test start: post";./owncloud.sh post test.txt testdir/test.txt || echo "owncloud.sh post ERROR"
+echo "test start: ls";./owncloud.sh ls testdir || echo "owncloud.sh ls ERROR:"
+echo "test start: get";./owncloud.sh get testdir/test.txt || echo "owncloud.sh get ERROR:"

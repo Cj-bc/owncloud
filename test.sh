9c00d514b838979cd2e276b4a4f853fa657306c1
Cj <cj-bc@y-modify.org>
2018-02-23 22:21:14 +0900

    Changed: added test of mkdir

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "this is test file!!!! hello world" > test.txt && echo "test: test.txt generated"
+echo "test start: mkdir";./owncloud.sh mkdir testdir || echo "owncloud.sh mkdir ERROR"
 echo "test start: post";./owncloud.sh post test.txt testdir/test.txt || echo "owncloud.sh post ERROR"
 echo "test start: ls";./owncloud.sh ls testdir || echo "owncloud.sh ls ERROR:"
 echo "test start: get";./owncloud.sh get testdir/test.txt || echo "owncloud.sh get ERROR:"

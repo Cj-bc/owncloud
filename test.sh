fd5719bf4f9b19f8ad8efc6d4e5677cf47e432fa
Cj <cj-bc@y-modify.org>
2018-02-23 19:18:20 +0900

    Fix: fix owncloud path (add .sh at end )

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "this is test file!!!! hello world" > test.txt
-./owncloud post test.txt testdir/test.txt || echo "owncloud post ERROR"
-./owncloud ls testdir || echo "owncloud ls ERROR:"
-./owncloud get testdir/test.txt || echo "owncloud get ERROR:"
+./owncloud.sh post test.txt testdir/test.txt || echo "owncloud.sh post ERROR"
+./owncloud.sh ls testdir || echo "owncloud.sh ls ERROR:"
+./owncloud.sh get testdir/test.txt || echo "owncloud.sh get ERROR:"

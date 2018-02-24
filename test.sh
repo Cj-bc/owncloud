cd7f17fcce8396151c59c4bc23cc8afd39f9ba86
Cj <cj-bc@y-modify.org>
2018-02-24 11:04:10 +0900

    Fix: remove surplus texts
    ./owncloud might detect all things below as option

--- test.sh
+++ test.sh
@@ -4,12 +4,12 @@ cache=0
 [ $# -ne 0 ] && [ $1 == '--cache' ] && cache=1
 
 echo "this is test file!!!! hello world" > test.txt && echo "test: test.txt generated"
-echo "test start: mkdir";./owncloud.sh mkdir testdir && echo "test success: mkdir" || echo "owncloud.sh mkdir ERROR"
-echo "test start: post";./owncloud.sh post test.txt testdir/test.txt && echo "test success:post" || echo "owncloud.sh post ERROR"
-echo "test start: ls";./owncloud.sh ls testdir && echo "test success: ls" || echo "owncloud.sh ls ERROR:"
-echo "test start: get";./owncloud.sh get testdir/test.txt && echo "test success:get" || echo "owncloud.sh get ERROR:"
+echo "test start: mkdir";./owncloud.sh mkdir testdir
+echo "test start: post";./owncloud.sh post test.txt testdir/test.txt
+echo "test start: ls";./owncloud.sh ls testdir
+echo "test start: get";./owncloud.sh get testdir/test.txt
 if [ $cache -ne 1 ]
 then
-  echo "test start: rm";./owncloud.sh rm testdir && echo "test success:rm" || echo "owncloud.sh rm ERROR:"
+  echo "test start: rm";./owncloud.sh rm testdir
   rm test.txt && echo "test: test.txt removed"
 fi

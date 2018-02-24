655e59e077e450b865c378a80cf4348bbd552294
Cj <cj-bc@y-modify.org>
2018-02-24 12:09:18 +0900

    Fix: replace '' around $USER:$PASSWD with "",surround each values with {}
    '' prevend contents to be expanded.So $USER and $PASSWD wasn't expanded.
    By surround with {}, bash can recognize where is the end of the name of
    the value

--- func/basicfunc.sh
+++ func/basicfunc.sh
@@ -20,14 +20,14 @@ function usage {
 # get specified file
 function ownget {
   
-  response=`curl -X $API_GETFILE[0] -u '$USER:$PASSWD' "$API_GETFILE[1]/$1" -s > ${1##*/}`
+  response=`curl -X ${API_GETFILE[0]} -u "$USER:$PASSWD" "${API_GETFILE[1]}/$1" -s > ${1##*/}`
   echo $response
 }
 
 # ls specified directory
 function ownls {
 
-  response=`curl -X $API_GETFILELIST[0] -u '$USER:$PASSWD' "$API_GETFILELIST[1]/$1" -s`
+  response=`curl -X ${API_GETFILELIST[0]} -u "$USER:$PASSWD" "${API_GETFILELIST[1]}/$1" -s`
   echo $response
 }
 
@@ -37,7 +37,7 @@ function ownls {
 #   -- file itself
 function ownpost {
 
-  response=`curl -X $API_POSTFILE[0] -u '$USER:$PASSWD' "$API_POSTFILE[1]/$1" --data-binary @$2`
+  response=`curl -X ${API_POSTFILE[0]} -u "$USER:$PASSWD" "${API_POSTFILE[1]}/$1" --data-binary @$2`
 
   [ $? -ne 0 ] && echo "ERROR: POSTING WAS FAILED" || echo "File is posted correctory." # if error has occured, output it.if not, output succeed
   [ "$response" != "" ] && echo "ERROR: POSTING WAS FAILED" || echo "File is posted correctory." # if error has occured, output it.if not, output succeed
@@ -46,7 +46,7 @@ function ownpost {
 
 function ownmkdir {
 
-  response=`curl -X $API_MKDIR[0] -u '$USER:$PASSWD' "$API_MKDIR[1]/$1"`
+  response=`curl -X ${API_MKDIR[0]} -u "$USER:$PASSWD" "${API_MKDIR[1]}/$1"`
 
   [ $? -ne 0 ] && echo "ERROR: FAILED TO MKDIR $1" || echo "Make specified directory correctory." # if error has occured, output it.if not, output succeed
   [ "$response" != "" ] && echo "ERROR: FAILED TO MKDIR $1" || echo "Make specified directory correctory." # if error has occured, output it.if not, output succeed
@@ -55,7 +55,7 @@ function ownmkdir {
 
 function ownrm {
 
-  response=`curl -X $API_RMFILE[0] -u '$USER:$PASSWD' "$API_RMFILE[1]/$1"`
+  response=`curl -X ${API_RMFILE[0]} -u "$USER:$PASSWD" "${API_RMFILE[1]}/$1"`
 
 
   [ "$response" != "" ] && echo "ERROR: FAILED TO remove $1" || echo "Remove specified directory correctory." # if error has occured, output it.if not, output succeed

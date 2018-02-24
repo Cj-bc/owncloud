6beead35ac892b28fb9e3bf25a64ea6d5919942a
Cj <cj-bc@y-modify.org>
2018-02-24 10:54:23 +0900

    Fix: warm if curl fail

--- func/basicfunc.sh
+++ func/basicfunc.sh
@@ -39,6 +39,7 @@ function ownpost {
 
   response=`curl -X $API_POSTFILE[0] -u $USER:$PASSWD "$API_POSTFILE[1]/$1" --data-binary @$2`
 
+  [ $? -ne 0 ] && echo "ERROR: POSTING WAS FAILED" || echo "File is posted correctory." # if error has occured, output it.if not, output succeed
   [ "$response" != "" ] && echo "ERROR: POSTING WAS FAILED" || echo "File is posted correctory." # if error has occured, output it.if not, output succeed
 }
 
@@ -47,13 +48,14 @@ function ownmkdir {
 
   response=`curl -X $API_MKDIR[0] -u $USER:$PASSWD "$API_MKDIR[1]/$1"`
 
+  [ $? -ne 0 ] && echo "ERROR: FAILED TO MKDIR $1" || echo "Make specified directory correctory." # if error has occured, output it.if not, output succeed
   [ "$response" != "" ] && echo "ERROR: FAILED TO MKDIR $1" || echo "Make specified directory correctory." # if error has occured, output it.if not, output succeed
 }
 
 
 function ownrm {
 
-  response=`curl -X $API_RMFILE[0] -u $USER:$PASSWD "$API_RMFILE[1]/$1"
+  response=`curl -X $API_RMFILE[0] -u $USER:$PASSWD "$API_RMFILE[1]/$1"`
 
 
   [ "$response" != "" ] && echo "ERROR: FAILED TO remove $1" || echo "Remove specified directory correctory." # if error has occured, output it.if not, output succeed

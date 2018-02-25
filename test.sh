a351c65093b6e4a72da6e3dbab15496c6416ad4c
Cj-bc <cj-bc@y-modify.org>
2018-02-25 17:46:55 +0900

    Change: show what was downloaded from cloud (at "get" test)

--- test.sh
+++ test.sh
@@ -8,6 +8,7 @@ echo "test start: mkdir";./owncloud.sh mkdir testdir
 echo "test start: post";./owncloud.sh post testdir/test.txt test.txt
 echo "test start: ls";./owncloud.sh ls testdir
 echo "test start: get";./owncloud.sh get testdir/test.txt
+echo "data:\r";cat test.txt
 if [ $cache -ne 1 ]
 then
   echo "test start: rm";./owncloud.sh rm testdir

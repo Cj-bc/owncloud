09fdfba4b8d7d94bdef1dbd4c0789e8c0e4759fd
Cj-bc <cj-bc@y-modify.org>
2018-02-28 15:08:42 +0900

    Change: test.sh> added test for mv

--- test.sh
+++ test.sh
@@ -7,10 +7,11 @@ echo "this is test file!!!! hello world" > test.txt && echo "test: test.txt gene
 echo "test start: mkdir";./owncloud.sh --local mkdir testdir
 echo "test start: post";./owncloud.sh --local post testdir/test.txt test.txt
 echo "test start: ls";./owncloud.sh --local ls testdir
-echo "test start: get";./owncloud.sh --local get testdir/test.txt
-echo "data:\r";cat test.txt
+echo "test start: mv";./owncloud.sh --local mv testdir/test.txt test.txt
+echo "test start: get";./owncloud.sh --local get test.txt
+echo "data:\n>  ";cat test.txt
 if [ $cache -ne 1 ]
 then
-  echo "test start: rm";./owncloud.sh --local rm testdir
+  echo "test start: rm";./owncloud.sh --local rm testdir;./owncloud.sh --local rm test.txt
   rm test.txt && echo "test: test.txt removed"
 fi

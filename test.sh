2f6ec8e77f572da93beed6983111a7d073dd88e6
Cj-bc <cj-bc@y-modify.org>
2018-02-28 13:25:53 +0900

    Change: >test.sh added --local option for each time

--- test.sh
+++ test.sh
@@ -4,13 +4,13 @@ cache=0
 [ $# -ne 0 ] && [ $1 == '--cache' ] && cache=1
 
 echo "this is test file!!!! hello world" > test.txt && echo "test: test.txt generated"
-echo "test start: mkdir";./owncloud.sh mkdir testdir
-echo "test start: post";./owncloud.sh post testdir/test.txt test.txt
-echo "test start: ls";./owncloud.sh ls testdir
-echo "test start: get";./owncloud.sh get testdir/test.txt
+echo "test start: mkdir";./owncloud.sh --local mkdir testdir
+echo "test start: post";./owncloud.sh --local post testdir/test.txt test.txt
+echo "test start: ls";./owncloud.sh --local ls testdir
+echo "test start: get";./owncloud.sh --local get testdir/test.txt
 echo "data:\r";cat test.txt
 if [ $cache -ne 1 ]
 then
-  echo "test start: rm";./owncloud.sh rm testdir
+  echo "test start: rm";./owncloud.sh --local rm testdir
   rm test.txt && echo "test: test.txt removed"
 fi

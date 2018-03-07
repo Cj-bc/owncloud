2b6417a9283dc5e156dfeb9d78e64a5f0fe9f097
Cj-bc <cj-bc@y-modify.org>
2018-03-07 14:34:54 +0900

    Add: basicconf> add errorcheck by error_API to all methods

--- func/basicfunc.sh
+++ func/basicfunc.sh
@@ -38,8 +38,10 @@ function ownconfig {
 # get specified file
 function ownget {
   
-  response=`curl -X ${API_GETFILE[0]} -u "$USER:$PASSWD" -s "${API_GETFILE[1]}/$1" > ${1##*/}`
-  echo $response
+  response=`curl -X ${API_GETFILE[0]} -u "$USER:$PASSWD" -s "${API_GETFILE[1]}/$1"`
+  [ "`error_API "$response";echo $?`" != "0" ] && echo $ERROR_APIRETURNERROR && return $NumERROR_APIRETURNERROR
+  echo $response > ${1##*/}
+  return $SUCCESS
 }
 
 # ls specified directory
@@ -48,6 +50,8 @@ function ownls {
   response=$(mktemp "/tmp/${0##*/}.tmp.XXXXXX")
   [ $# -eq 0 ] && local path="" || local path=$1
   curl -X ${API_GETFILELIST[0]} -u "$USER:$PASSWD" -s "${API_GETFILELIST[1]}/$path" -o "$response"
+
+  [ "`error_API $response;echo $?`" != "0" ] && echo $ERROR_APIRETURNERROR && return $NumERROR_APIRETURNERROR
 
   ex -s "$response" <<-EOT
     %s/</\r</g
@@ -63,8 +67,8 @@ function ownls {
     sort
     w!
 EOT
-  cat "$response"
 
+  cat $response
   rm "$response"
 }
 
@@ -77,7 +81,11 @@ function ownpost {
   response=`curl -X ${API_POSTFILE[0]} -u "$USER:$PASSWD" -s "${API_POSTFILE[1]}/$2" --data-binary @$1`
 
   [ $? -ne 0 ] && echo -e "error: something wrong with CURL.\nusage: post <file> <dest>" # if error has occured, output it.if not, output succeed
-  [ "$response" != "" ] && echo "ERROR: POSTING WAS FAILED" || echo "\"$1\" is posted correctory." # if error has occured, output it.if not, output succeed
+  [ "`error_API $response;echo $?`" != "0" ] && echo $ERROR_APIRETURNERROR && return $NumERROR_APIRETURNERROR
+
+  echo "\"$1\" is posted correctory." # if error has occured, output it.if not, output succeed
+  return $SUCCESS
+
 }
 
 
@@ -86,7 +94,9 @@ function ownmkdir {
   response=`curl -X ${API_MKDIR[0]} -u "$USER:$PASSWD" -s "${API_MKDIR[1]}/$1"`
 
   [ $? -ne 0 ] && echo -e "error: something wrong with CURL.\nusage: mkdir <dir>" # if error has occured, output it.if not, output succeed
-  [ "$response" != "" ] && echo -e "ERROR: FAILED TO MKDIR $1.Response is below:\n\n${response}" || echo "Make \"$1\" correctory." # if error has occured, output it.if not, output succeed
+  [ "`error_API $response;echo $?`" != "0" ] && echo $ERROR_APIRETURNERROR && return $NumERROR_APIRETURNERROR
+ echo "Make \"$1\" correctory." # if error has occured, output it.if not, output succeed
+ return $SUCCESS
 }
 
 
@@ -95,7 +105,9 @@ function ownrm {
   response=`curl -X ${API_RMFILE[0]} -u "$USER:$PASSWD" -s "${API_RMFILE[1]}/$1"`
 
 
-  [ "$response" != "" ] && echo "ERROR: FAILED TO remove $1" || echo "Remove \"$1\" correctory." # if error has occured, output it.if not, output succeed
+  [ "`error_API $response;echo $?`" != "0" ] && echo $ERROR_APIRETURNERROR && return $NumERROR_APIRETURNERROR
+  echo "Remove \"$1\" correctory." # if error has occured, output it.if not, output succeed
+  return $SUCCESS
 }
 
 
@@ -103,15 +115,16 @@ function ownmv {
   
   response=`curl -X ${API_MVFILE[0]} -u "$USER:$PASSWD" -s --header "${DestURL}/$2" "${API_MVFILE[1]}/$1"`
    
-  echo $response
-  [ "$response" != "" ] && echo "ERROR: FAILED TO move $1" || echo "Move \"$1\" correctory." # if error has occured, output it.if not, output succeed
+  [ "`error_API $response;echo $?`" != "0" ] && echo $ERROR_APIRETURNERROR && return $NumERROR_APIRETURNERROR
+  echo "Move \"$1\" correctory." # if error has occured, output it.if not, output succeed
+  return $SUCCESS
 }
 
 function owncp {
 
   response=`curl -X ${API_CPFILE[0]} -u "$USER:$PASSWD" -s --header "${DestURL}/$2" "${API_CPFILE[1]}/$1"`
 
-  echo $response
-  [ "$response" != "" ] && echo "ERROR: FAILED TO copy $1" || echo "Copy \"$1\" correctory." # if error has occured, output it.if not, output succeed
-
+  [ "`error_API $response;echo $?`" != "0" ] && echo $ERROR_APIRETURNERROR && return $NumERROR_APIRETURNERROR
+  echo "Copy \"$1\" correctory." # if error has occured, output it.if not, output succeed
+  return $SUCCESS
 }

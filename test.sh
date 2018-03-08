c6ca1d67db96e95a8393cd060a65e1f5eed891ee
Cj-bc <cj-bc@y-modify.org>
2018-03-08 14:52:26 +0900

    Fix: test.sh> change owncloud.sh to owncloud(for non local test)

--- test.sh
+++ test.sh
@@ -7,15 +7,15 @@ cache=0;localflag=0
 case $localflag in
   0)
     echo "this is test file!!!! hello world" > test.txt && echo "test: test.txt generated"
-    echo "test start: mkdir";owncloud.sh  mkdir testdir
-    echo "test start: post";owncloud.sh  post test.txt testdir/test.txt
-    echo "test start: ls";owncloud.sh  ls testdir
-    echo "test start: mv";owncloud.sh  mv testdir/test.txt test.txt
-    echo "test start: get";owncloud.sh  get test.txt
+    echo "test start: mkdir";owncloud  mkdir testdir
+    echo "test start: post";owncloud post test.txt testdir/test.txt
+    echo "test start: ls";owncloud  ls testdir
+    echo "test start: mv";owncloud  mv testdir/test.txt test.txt
+    echo "test start: get";owncloud  get test.txt
     echo "data:\n>  ";cat test.txt
     if [ $cache -ne 1 ]
     then
-      echo "test start: rm";owncloud.sh rm testdir;owncloud.sh rm test.txt
+      echo "test start: rm";owncloud rm testdir;owncloud rm test.txt
       rm test.txt && echo "test: test.txt removed"
     fi
     ;;

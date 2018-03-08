cb1c24536bdc594d23ce135e3d3676e753b83256
Cj-bc <cj-bc@y-modify.org>
2018-03-08 12:46:55 +0900

    Change: test.sh> cope with --local flag. now execute installed owncloud by default

--- test.sh
+++ test.sh
@@ -1,17 +1,36 @@
 #!/bin/bash
 
-cache=0
-[ $# -ne 0 ] && [ $1 == '--cache' ] && cache=1
+cache=0;localflag=0
+[ $# -ne 0 ] && [ $1 == '--cache' ] && cache=1 && shift
+[ $# -ne 0 ] && [ $1 == '--local' ] && localflag=1
 
-echo "this is test file!!!! hello world" > test.txt && echo "test: test.txt generated"
-echo "test start: mkdir";./owncloud.sh --local mkdir testdir
-echo "test start: post";./owncloud.sh --local post test.txt testdir/test.txt
-echo "test start: ls";./owncloud.sh --local ls testdir
-echo "test start: mv";./owncloud.sh --local mv testdir/test.txt test.txt
-echo "test start: get";./owncloud.sh --local get test.txt
-echo "data:\n>  ";cat test.txt
-if [ $cache -ne 1 ]
-then
-  echo "test start: rm";./owncloud.sh --local rm testdir;./owncloud.sh --local rm test.txt
-  rm test.txt && echo "test: test.txt removed"
-fi
+case $localflag in
+  0)
+    echo "this is test file!!!! hello world" > test.txt && echo "test: test.txt generated"
+    echo "test start: mkdir";owncloud.sh  mkdir testdir
+    echo "test start: post";owncloud.sh  post test.txt testdir/test.txt
+    echo "test start: ls";owncloud.sh  ls testdir
+    echo "test start: mv";owncloud.sh  mv testdir/test.txt test.txt
+    echo "test start: get";owncloud.sh  get test.txt
+    echo "data:\n>  ";cat test.txt
+    if [ $cache -ne 1 ]
+    then
+      echo "test start: rm";owncloud.sh rm testdir;owncloud.sh rm test.txt
+      rm test.txt && echo "test: test.txt removed"
+    fi
+    ;;
+  1)
+    echo "this is test file!!!! hello world" > test.txt && echo "test: test.txt generated"
+    echo "test start: mkdir";./owncloud.sh --local mkdir testdir
+    echo "test start: post";./owncloud.sh --local post test.txt testdir/test.txt
+    echo "test start: ls";./owncloud.sh --local ls testdir
+    echo "test start: mv";./owncloud.sh --local mv testdir/test.txt test.txt
+    echo "test start: get";./owncloud.sh --local get test.txt
+    echo "data:\n>  ";cat test.txt
+    if [ $cache -ne 1 ]
+    then
+      echo "test start: rm";./owncloud.sh --local rm testdir;./owncloud.sh --local rm test.txt
+      rm test.txt && echo "test: test.txt removed"
+    fi
+    ;;
+esac

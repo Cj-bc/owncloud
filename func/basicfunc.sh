14cd99a3da07d9b63db759ab1dc628d81e25c481
Cj-bc <cj-bc@y-modify.org>
2018-03-15 20:05:24 +0900

    Fix: ownget> avoid response=(nothing)
    surround response with ""

--- func/basicfunc.sh
+++ func/basicfunc.sh
@@ -38,8 +38,8 @@ function ownconfig {
 # get specified file
 function ownget {
   
-  response=`curl -X ${API_GETFILE[0]} -u "$USER:$PASSWD" -s "${API_GETFILE[1]}/$1"`
   [ "`error_API "$response";echo $?`" != "0" ] && echo $ERROR_APIRETURNERROR && return $NumERROR_APIRETURNERROR
+  response="`curl -X ${API_GETFILE[0]} -u "$USER:$PASSWD" -s "${API_GETFILE[1]}/$1"`"
   echo $response > "${1##*/}"
   return $SUCCESS
 }
